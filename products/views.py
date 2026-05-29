bc0662d9cbe3418a40f8a23c8c565975ce22bace
c0ntl1e <vladstasukevic147@gmail.com>
2026-05-29 18:29:22 +0300

    Add Allegro style filters to products page

--- products/views.py
+++ products/views.py
@@ -98,8 +98,98 @@ def homepage(request):
     })
 
 
+from django.core.paginator import Paginator
+
 def product_list(request):
-    return homepage(request)
+
+    products = Product.objects.select_related(
+        'company',
+        'category'
+    ).all()
+
+    categories = Category.objects.all()
+
+    companies = []
+
+    for product in Product.objects.select_related('company'):
+        if product.company not in companies:
+            companies.append(product.company)
+
+    query = request.GET.get('q', '')
+    category = request.GET.get('category')
+    company = request.GET.get('company')
+    sort = request.GET.get('sort')
+
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    if query:
+        products = products.filter(
+            name__icontains=query
+        )
+
+    if category and category != 'None':
+        products = products.filter(
+            category_id=category
+        )
+
+    if company and company != 'None':
+        products = products.filter(
+            company_id=company
+        )
+
+    if min_price:
+        products = products.filter(
+            price__gte=min_price
+        )
+
+    if max_price:
+        products = products.filter(
+            price__lte=max_price
+        )
+
+    if sort == 'price_asc':
+        products = products.order_by('price')
+
+    elif sort == 'price_desc':
+        products = products.order_by('-price')
+
+    elif sort == 'oldest':
+        products = products.order_by('id')
+
+    else:
+        products = products.order_by('-id')
+
+    category_counts = {}
+
+    for cat in categories:
+        category_counts[cat.id] = Product.objects.filter(
+            category=cat
+        ).count()
+
+    paginator = Paginator(products, 8)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'products/product_list.html',
+        {
+            'products': page_obj,
+            'page_obj': page_obj,
+            'categories': categories,
+            'companies': companies,
+            'category_counts': category_counts,
+            'query': query,
+            'selected_category': category,
+            'selected_company': company,
+            'selected_sort': sort,
+            'min_price': min_price,
+            'max_price': max_price,
+        }
+    )
 
 
 def product_detail(request, pk):
